bcd39f433c33207e42a978b7c299fb841382ed4b
oladipupo Aisha <oladipupoayomide100@gmail.com>
2019-06-07 11:35:47 +0100

    Add files via upload

---
+++ db.js
@@ -0,0 +1,67 @@
+const posts =[
+    {
+        id:1,
+    title:"my holiday with Aisha hbngzh vhgzjn",
+    image ="../images/sampleimages/capture.Png",
+    description:"lorem bhvdcfut asdtsa fSYTgs Lorem ipsum dolor sit amet consectetur adipisicing elit.  Eligendi, culpa aut maxime, nulla accusantium voluptate",
+    },
+
+
+    {
+        id:2,
+        title:"my lovely baby j hfdbhgjn hbzust",
+        image:"../images/sampleimages/capture.Png",
+        description:"lolorem bhvdcfut asdtsa fSYTgs Lorem ipsum dolor sit amet consectetur adipisicing elit.  Eligendi, culpa aut maxime, nulla accusantium voluptate"
+    },
+
+
+    {
+        id:3,
+        title:"my lovely baby hug him just",
+        image:"../images/sampleimages/capture.Png",
+        description:"lolorem bhvdcfut asdtsa fSYTgs Lorem ipsum dolor sit amet consectetur adipisicing elit.  Eligendi, culpa aut maxime, nulla accusantium voluptate"
+    },
+
+    {
+        id:4,
+        title:"my lovely baby just hyuggbsd gzsjhu",
+        image:"../images/sampleimages/capture.Png",
+        description:"lolorem bhvdcfut asdtsa fSYTgs Lorem ipsum dolor sit amet consectetur adipisicing elit.  Eligendi, culpa aut maxime, nulla accusantium voluptate"
+    },
+
+    {
+        id:5,
+        title:"my lovely baby just hdhboj hdhu",
+        image:"../images/sampleimages/capture.Png",
+        description:"lolorem bhvdcfut asdtsa fSYTgs Lorem ipsum dolor sit amet consectetur adipisicing elit.  Eligendi, culpa aut maxime, nulla accusantium voluptate"
+    },
+
+    {
+        id:6,
+        title:"my lovely baby just lorem ipsum",
+        image:"../images/sampleimages/capture.Png",
+        description:"lolorem bhvdcfut asdtsa fSYTgs Lorem ipsum dolor sit amet consectetur adipisicing elit.  Eligendi, culpa aut maxime, nulla accusantium voluptate"
+    },
+
+    {
+        id:7,
+        title:"my lovely baby just puked",
+        image:"../images/sampleimages/capture.Png",
+        description:"lolorem bhvdcfut asdtsa fSYTgs Lorem ipsum dolor sit amet consectetur adipisicing elit.  Eligendi, culpa aut maxime, nulla accusantium voluptate"
+    },
+
+    {
+        id:8,
+        title:"my lovely gf just broke up",
+        image:"../images/sampleimages/capture.Png",
+        description:"lolorem bhvdcfut asdtsa fSYTgs Lorem ipsum dolor sit amet consectetur adipisicing elit.  Eligendi, culpa aut maxime, nulla accusantium voluptate"
+    },
+
+    {
+        id:9t,
+        title:"my lovely wife is ",
+        image:"../images/sampleimages/capture.Png",
+        description:"lolorem bhvdcfut asdtsa fSYTgs Lorem ipsum dolor sit amet consectetur adipisicing elit.  Eligendi, culpa aut maxime, nulla accusantium voluptate"
+    },
+
+]
